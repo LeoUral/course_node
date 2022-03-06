5b1d7e837f15e49e62265352f48f04fe6a29b9ee
Leonid Kudryashov <leoural66@yandex.ru>
2022-03-06 22:01:01 +0500

    правка URL

--- frontend-src/api.js
+++ frontend-src/api.js
@@ -1,4 +1,5 @@
-const PREFIX = "http://localhost:3000/dashboard";
+// const PREFIX = "http://localhost:3000/dashboard";
+const PREFIX = "https://mykudryashov.ru:3000/dashboard";
 
 const req = async(url, options = {}) => {
     const { body } = options;
